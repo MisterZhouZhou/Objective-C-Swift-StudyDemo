5bd0b6892c380981d27e48a7dd13e031a4207f38
小溪彼岸 <m15038960016@163.com>
2016-06-02 10:28:14 +0800

    添加了空工程

---
+++ Objective-C-Demo/Objective-C-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Objective-C-Demo
+//
+//  Created by rayootech on 16/6/2.
+//  Copyright © 2016年 rayootech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-C-Demo/Objective-C-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objective-C-Demo
+//
+//  Created by rayootech on 16/6/2.
+//  Copyright © 2016年 rayootech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
